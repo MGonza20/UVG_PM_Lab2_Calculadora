6bc8db4a3d009b7abad55137be685f46f762baa9
MGonza20 <pag20634@uvg.edu.gt>
2021-05-06 10:42:15 -0600

    .

--- app/src/main/java/com/example/pm_lab2_calcu/Calculadora.kt
+++ app/src/main/java/com/example/pm_lab2_calcu/Calculadora.kt
@@ -3,9 +3,9 @@ package com.example.pm_lab2_calcu
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity2 : AppCompatActivity() {
+class Calculadora : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_calculadora)
     }
 }
--- app/src/main/java/com/example/pm_lab2_calcu/MainActivity.kt
+++ app/src/main/java/com/example/pm_lab2_calcu/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.pm_lab2_calcu
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +12,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show()
+
+        val btnHello: Button = findViewById(R.id.btnHello)
+
+        btnHello.setOnClickListener {
+            val intent: Intent = Intent(this, Calculadora::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
+
 
     override fun onStart() {
         super.onStart()
